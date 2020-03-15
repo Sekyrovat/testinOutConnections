a47aad349b8c1e46edb1a1c8f616e280aedfe745
Hector Hernandez Morales <hectorhm1596@gmail.com>
2020-03-15 17:28:46 -0400

    Update testOutConnectivity.py

--- testOutConnectivity.py
+++ testOutConnectivity.py
@@ -2,7 +2,7 @@ import socket
 import time
 import sys, getopt
 
-def TestConnectivity(ip, port, num_tries=100):
+def TestConnectivity(ip, port, num_tries):
   try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   except socket.error as err: 
@@ -17,9 +17,9 @@ def TestConnectivity(ip, port, num_tries=100):
       return (raised_errno, msg)
     return (0, 'Success')
 
-def WaitForNetwork(ip, port, total_wait=20, totalTries=100, outFileName="logs.txt"):
-  logFile = open(outFileName, "x")
-  logFile.write('Beggining time='+str(time.time()))
+def WaitForNetwork(ip, port, total_wait, totalTries=100, outFileName="logs.txt"):
+  logFile = open(outFileName, "w")
+  logFile.write('Beggining time='+str(time.time())+'\n')
   end = time.time() + total_wait
   err, msg = TestConnectivity(ip, port, totalTries)
   while err and time.time() <= end:
@@ -27,36 +27,36 @@ def WaitForNetwork(ip, port, total_wait=20, totalTries=100, outFileName="logs.tx
     time.sleep(0.5)
     err, msg = TestConnectivity(ip, port, totalTries)
   if err:
-    logFile.write("\n\nAll failed")
+    logFile.write("\n\nAll failed "+str(time.time()))
   else:
-    logFile.write('\n\n\nVerified full external network connectivity for VM\n\n')
+    logFile.write('\n\n\nVerified full external network connectivity for VM at '+str(time.time())+'\n\n')
   logFile.close()
 
 def main(argv):
   ip = "127.0.0.1"
   port = 80
-  waitTime = 20
+  waitTime = 60
   totalTries = 100
   outFileName = ''
   try:
-      opts, args = getopt.getopt(sys.argv[1:],"hpwti:o:")
+    opts, args = getopt.getopt(sys.argv[1:],"hp:w:t:i:o:")
   except getopt.GetoptError:
-    print('testOutConnectivity.py\n-i IP\n-o Outfile\n')
+    print('testOutConnectivity.py\n-i IP\n-p Port\n-w Wait\n-t Total attempts\n-o Outfile\n')
     sys.exit(2)
   for opt, arg in opts:
-      if opt == '-h':
-         print('testOutConnectivity.py\n-i IP\n-p Port\n-w Wait\n-t Total attempts\n-o Outfile\n')
-         sys.exit()
-      elif opt in ("-i"):
-         ip = arg
-      elif opt in ("-p"):
-         port = arg
-      elif opt in ("-w"):
-         waitTime = arg
-      elif opt in ("-t"):
-         totalTries = arg
-      elif opt in ("-o"):
-         outFileName = arg
+    if opt == '-h':
+      print('testOutConnectivity.py\n-i IP\n-p Port\n-w Wait\n-t Total attempts\n-o Outfile\n')
+      sys.exit()
+    elif opt in ("-i"):
+      ip = arg
+    elif opt in ("-p"):
+      port = int(arg)
+    elif opt in ("-w"):
+      waitTime = int(arg)
+    elif opt in ("-t"):
+      totalTries = int(arg)
+    elif opt in ("-o"):
+      outFileName = arg
   WaitForNetwork(ip, port, waitTime, totalTries, outFileName)
 
 if __name__ == "__main__":
